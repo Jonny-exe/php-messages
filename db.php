f7d1757e1eb296368b780d812cdd081756107df6
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-03-03 18:31:25 +0100

    Updated db queries to avoid sql injection

--- db.php
+++ db.php
@@ -1,11 +1,24 @@
 <?php
 
 // Database functions 
+// function does_user_exist1($conn, $name)
+// {
+//     $q = "SELECT * from users where name = '$name'";
+//     $result = $conn->query($q);
+//     $result = $result->fetchAll();
+//     if (count($result) == 0) {
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+
 function does_user_exist($conn, $name)
 {
-    $q = "SELECT * from users where name = '$name'";
-    $result = $conn->query($q);
-    $result = $result->fetchAll();
+    $q = "SELECT * from users where name = ?";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($name));
+    $result = $prepare->fetchAll();
     if (count($result) == 0) {
         return false;
     } else {
@@ -15,9 +28,10 @@ function does_user_exist($conn, $name)
 
 function is_friend_already_added($conn, $uid, $friend_name)
 {
-    $q = "SELECT * from friends where name = '$friend_name' and uid = $uid";
-    $result = $conn->query($q);
-    $result = $result->fetchAll();
+    $q = "SELECT * from friends where name = ? and uid = ?";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($friend_name, $uid));
+    $result = $prepare->fetchAll();
     if (count($result) == 0) {
         return false;
     } else {
@@ -27,9 +41,10 @@ function is_friend_already_added($conn, $uid, $friend_name)
 
 function does_friend_exist($conn, $friend_name)
 {
-    $q = "SELECT * from users where name = '$friend_name'";
-    $result = $conn->query($q);
-    $result = $result->fetchAll();
+    $q = "SELECT * from users where name = ?";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($friend_name));
+    $result = $prepare->fetchAll();
     if (count($result) == 0) {
         return false;
     } else {
@@ -40,36 +55,43 @@ function does_friend_exist($conn, $friend_name)
 
 function get_messages($conn, $friend, $name)
 {
-    $q = "select sender_text, receiver_text, sender from messages where (sender='$name' and receiver='$friend') or (sender='$friend' and receiver='$name')";
-    $messages = $conn->query($q);
-    $messages = $messages->fetchAll();
-    return $messages;
+    $q = "select sender_text, receiver_text, sender from messages where (sender=? and receiver=?) or (sender=? and receiver=?)";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($name, $friend, $friend, $name));
+    $result = $prepare->fetchAll();
+    return $result;
 }
 
 function insert_user($conn, $name, $public_key)
 {
-    $q = "insert into users (name, public_key) values ('$name', '$public_key')";
-    $conn->query($q);
+    $q = "insert into users (name, public_key) values (?, ?)";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($name, $public_key));
 }
 
 function insert_friend($conn, $uid, $friend_name)
 {
-    $q = "insert into friends (name, uid) values ('$friend_name', $uid)";
-    $conn->query($q);
+    $q = "insert into friends (name, uid) values (?, ?)";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($friend_name, $uid));
 }
 
 function insert_message($conn, $sender_text, $receiver_text, $sender, $receiver)
 {
     $sender_text = sodium_bin2base64($sender_text, 1);
     $receiver_text = sodium_bin2base64($receiver_text, 1);
-    $q = "insert into messages (sender_text, receiver_text, sender, receiver) values ('$sender_text','$receiver_text', '$sender', '$receiver')";
-    $conn->query($q);
+    $q = "insert into messages (sender_text, receiver_text, sender, receiver) values (?,?,?,?)";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($sender_text, $receiver_text, $sender, $receiver));
 }
 
 function get_public_key($name, $conn, $bin = true)
 {
-    $result = $conn->query("SELECT public_key from users where name = '$name'");
-    $result = $result->fetch()[0];
+    //TODO: change $conn, to first place
+    $q = "SELECT public_key from users where name = ?";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($name));
+    $result = $prepare->fetch()[0];
     if ($bin == true) {
         $result = sodium_base642bin($result, 1);
     }
@@ -78,14 +100,19 @@ function get_public_key($name, $conn, $bin = true)
 
 function getUID($conn, $name)
 {
-    $uid = $conn->query("select uid from users where name='$name' order by uid desc");
-    $uid = $uid->fetch();
-    return $uid[0];
+    $q = "select uid from users where name=? order by uid desc";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($name));
+    $result = $prepare->fetch()[0];
+    return $result;
 }
 
 function get_friends($conn, $uid)
 {
-    $result = $conn->query("select name, uid from friends where uid=$uid");
+    $q = "select name, uid from friends where uid = ? ";
+    $result = $conn->prepare($q);
+    // $result = $conn->query($q);
+    $result->execute(array($uid));
     if (!$result) {
         return [];
     }
@@ -95,9 +122,11 @@ function get_friends($conn, $uid)
 
 function remove_friend($conn, $uid, $friend, $name)
 {
-    $q = "delete from friends where uid=$uid and name='$friend'";
-    $conn->query($q);
+    $q = "delete from friends where uid=? and name=?";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($uid, $friend));
 
-    $q = "delete from messages where (sender='$name' and receiver='$friend') or (sender='$friend' and receiver='$name')";
-    $conn->query($q);
+    $q = "delete from messages where (sender=? and receiver=?) or (sender=? and receiver=?)";
+    $prepare = $conn->prepare($q);
+    $prepare->execute(array($name, $friend, $friend, $name));
 }
